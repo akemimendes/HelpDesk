0608f2ae7e553009138ddf63119ebd835277b4a0
akemimendes <akemi.mendes@hotmail.com>
2025-07-17 07:15:16 -0300

    endpoint findById tecnico

--- helpdesk/helpdesk/src/main/java/com/curso/udemy/helpdesk/HelpdeskApplication.java
+++ helpdesk/helpdesk/src/main/java/com/curso/udemy/helpdesk/HelpdeskApplication.java
@@ -12,6 +12,5 @@ public class HelpdeskApplication {
 		SpringApplication.run(HelpdeskApplication.class, args);
 	}
 
-	
 
 }
--- helpdesk/helpdesk/src/main/java/com/curso/udemy/helpdesk/config/DevConfig.java
+++ helpdesk/helpdesk/src/main/java/com/curso/udemy/helpdesk/config/DevConfig.java
@@ -1,6 +1,7 @@
 package com.curso.udemy.helpdesk.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,16 +9,21 @@ import org.springframework.context.annotation.Profile;
 import com.curso.udemy.helpdesk.services.DBService;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 
     @Autowired
     private DBService dbService;
 
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String valor;
+
     @Bean
-    public Object instanciaDB(){
-        this.dbService.instanciaDB();
-        return dbService;
+    public boolean instanciaDB() {
+        if (valor.equals("create")) {
+            this.dbService.instanciaDB();
+        }
+        return false;
     }
 
 }
