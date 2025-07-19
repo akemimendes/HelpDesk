7c60d00f4035260a4a90623e15962f93044f47e5
akemimendes <akemi.mendes@hotmail.com>
2025-07-19 16:40:32 -0300

    Implementando delete Tecnico

--- helpdesk/helpdesk/src/main/java/com/curso/udemy/helpdesk/services/TecnicoService.java
+++ helpdesk/helpdesk/src/main/java/com/curso/udemy/helpdesk/services/TecnicoService.java
@@ -49,6 +49,14 @@ public class TecnicoService {
         return repository.save(oldObj);
     }
 
+     public void delete(Integer id) {
+        Tecnico obj=findById(id);
+        if (obj.getChamados().size()>0){
+            throw new DataIntegrityViolationException("Tecnico possui ordens de serviço e não pode ser deletado!");
+        }
+        repository.deleteById(id);
+    }
+
     private void validaPorCpfEmail(TecnicoDTO objDTO) {
         Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
         if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
@@ -59,6 +67,8 @@ public class TecnicoService {
             throw new DataIntegrityViolationException("Email já cadastrado no sistema!");
         }
     }
+
+   
 
     
 
