2bc023039005993c30c5023c99fae4e35a544b56
akemimendes <71134189+akemimendes@users.noreply.github.com>
2025-08-06 19:36:39 -0300

    Update JWTAuthenticationFilter.java

--- helpdesk/helpdesk/src/main/java/com/curso/udemy/helpdesk/security/JWTAuthenticationFilter.java
+++ helpdesk/helpdesk/src/main/java/com/curso/udemy/helpdesk/security/JWTAuthenticationFilter.java
@@ -54,7 +54,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         String token = jwtUtil.generateToken(username);
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, enctype, Location");
+        response.setHeader("Access-Control-Expose-Headers", "Authorization, Content-Type, enctype, Location");
         response.setHeader("Authorization", "Bearer " + token);
     }
 
